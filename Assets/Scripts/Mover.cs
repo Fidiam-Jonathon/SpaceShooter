41b889973bc90e777f4ccb04303752f0615b5faf
Jonathon Fidiam <Fid15001@byui.edu>
2018-07-08 00:12:50 -0700

    uploaded work-in-progress game.  Implemented move and shoot logic for the Player ship and the firing Bolts

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    public float speed;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+       
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = transform.forward * speed;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundry
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Boundry boundry;
+    public float tilt;
+    public GameObject shot;
+    public Transform ShotSpawn;
+    public float fireDelay;
+    private float nextFire;
+
+    private void Start()
+    {
+        speed     = 6;
+        nextFire  = 0.0f;
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal") * speed;
+        float moveVertical = Input.GetAxis("Vertical")     * speed;
+
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        rb.rotation = Quaternion.Euler(0, 0, rb.velocity.x * -tilt);
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.velocity = movement;
+
+        rb.position = new Vector3
+            (
+            Mathf.Clamp(rb.position.x, boundry.xMin, boundry.xMax),
+            0.0f,
+            Mathf.Clamp(rb.position.z, boundry.zMin, boundry.zMax)
+            );
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireDelay;
+            Instantiate(shot, ShotSpawn.position, ShotSpawn.rotation);
+            
+        }
+       
+    }
+}
+
